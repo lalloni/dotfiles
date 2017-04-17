9135eee19679329ac73a389ae92932359c1ea37d
Pablo Lalloni <plalloni@gmail.com>
2017-04-17 00:01:03 -0300

    Improve test script

--- test.sh
+++ test.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
+export DEBIAN_FRONTEND=noninteractive
 die() {
     exit 1
 }
-if [ "$1" != "run" ]; then
+if [ $# -eq 0 ]; then
     DISTS="zesty xenial trusty"
     for DIST in $DISTS; do
-        echo "################################################ Testing in $DIST"
-        docker run -ti --rm -v $PWD/$0:/test.sh "ubuntu:$DIST" /test.sh run || die
+        echo "################################################################## Testing in $DIST"
+        N="dotfiles-$DIST"
+        docker run -ti -v "$PWD/$0:/test.sh" --name "$N" "ubuntu:$DIST" /test.sh prepare || die
+        docker commit "$N" "$N" || die
+        docker rm -f "$N" || die
+        docker run -ti -v "$PWD/$0:/test.sh" --name "$N" "$N" /test.sh run || die
+        docker commit "$N" "$N" || die
+        docker rm -f "$N" || die
     done
     exit
 fi
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y locales sudo git lsb-release || die
-locale-gen --purge en_US.UTF-8 || die
-echo "LANG=\"en_US.UTF-8\"\nLANGUAGE=\"en_US:en\"" > /etc/default/locale
-echo "America/Argentina/Buenos_Aires" > /etc/timezone
-git clone https://github.com/lalloni/dotfiles ~/dotfiles || die
-~/dotfiles/install -y || die
-
+if [ "$1" = "prepare" ]; then
+    apt-get update
+    apt-get install -yq locales sudo git lsb-release || die
+    locale-gen --purge en_US.UTF-8 || die
+    echo "LANG=\"en_US.UTF-8\"\nLANGUAGE=\"en_US:en\"" > /etc/default/locale
+    echo "America/Argentina/Buenos_Aires" > /etc/timezone
+    exit
+fi
+if [ "$1" = "run" ]; then
+    export LANG="en_US.UTF-8"
+    export LANGUAGE="en_US:en"
+    git clone https://github.com/lalloni/dotfiles ~/dotfiles || die
+    ~/dotfiles/install -y || die
+    exit
+fi
+echo "Unknown test phase: $1"
+exit 1
