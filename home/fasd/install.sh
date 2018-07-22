8e1cee52a12fa0f3763e2ad28717d82fbf632f27
Pablo Lalloni <plalloni@gmail.com>
2018-07-22 15:24:38 -0300

    Add fasd, fzf, thefuck installation

---
+++ home/fasd/install.sh
@@ -0,0 +1,2 @@
+curl -#Lo "$HOME/bin/fasd" https://raw.githubusercontent.com/clvv/fasd/1.0.1/fasd
+chmod -v +x "$HOME/bin/fasd"
---
+++ home/thefuck/install.sh
@@ -0,0 +1,8 @@
+aptensurepkg virtualenv
+if [[ ! -d "$HOME/.py3env" ]]
+then
+    virtualenv --python=python3 "$HOME/.py3env"
+fi
+"$HOME/.py3env/bin/pip" install --upgrade thefuck
+ln -fsv "$HOME/.py3env/bin/fuck" "$HOME/bin/"
+ln -fsv "$HOME/.py3env/bin/thefuck" "$HOME/bin/"
