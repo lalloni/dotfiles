8cc3e0c8ce93877a46eb7b8b27ddc5308bd1d3c3
Pablo Lalloni <plalloni@gmail.com>
2018-07-26 17:37:08 -0300

    Improve thefuck installation

--- home/thefuck/install.sh
+++ home/thefuck/install.sh
@@ -1,8 +1,6 @@
-aptensurepkg virtualenv python3-dev
+aptensurepkg virtualenv build-essential python3-dev
 if [[ ! -d "$HOME/.py3env" ]]
 then
     virtualenv --python=python3 "$HOME/.py3env"
 fi
 "$HOME/.py3env/bin/pip" install --upgrade thefuck
-ln -fsv "$HOME/.py3env/bin/fuck" "$HOME/bin/"
-ln -fsv "$HOME/.py3env/bin/thefuck" "$HOME/bin/"
