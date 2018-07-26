1d6fb24f4b3db10e75de3f81626a81a0d1a41efe
Pablo Lalloni <plalloni@gmail.com>
2018-07-26 17:49:13 -0300

    Replace fasd and fzf installers fors run/install scripts

--- home/fasd/install.sh
+++
@@ -1,2 +0,0 @@
-curl -#Lo "$HOME/bin/fasd" https://raw.githubusercontent.com/clvv/fasd/1.0.1/fasd
-chmod -v +x "$HOME/bin/fasd"
--- home/fzf/install.sh
+++
@@ -1,6 +0,0 @@
-
-aptensurepkg curl
-
-curl -#Lo /tmp/fzf.tgz https://github.com/junegunn/fzf-bin/releases/download/0.17.4/fzf-0.17.4-linux_amd64.tgz
-
-tar -C ~/bin -xvzf /tmp/fzf.tgz
