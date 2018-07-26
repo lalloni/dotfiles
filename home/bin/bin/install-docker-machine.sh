1cb3702535b3156b42af8a906b2096ee9f30dd45
Pablo Lalloni <plalloni@gmail.com>
2018-07-26 17:16:56 -0300

    Replace docker-machine and minikube run scripts for install scripts
    New scripts transparently download and cache corresponding binaries.

--- home/bin/bin/install-docker-machine.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -Eeuo pipefail
-echo "Downloading docker-machine..."
-curl -#Lo "$HOME/bin/docker-machine" https://github.com/docker/machine/releases/download/v0.15.0/docker-machine-$(uname -s)-$(uname -m)
-chmod -v +x "$HOME/bin/docker-machine"
-#echo "Downloading docker-machine-kvm2..."
-#curl -#Lo "$HOME/bin/docker-machine-kvm2" https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2
-#chmod -v +x "$HOME/bin/docker-machine-kvm2"
