4de66bbeb7689274998c54c47a8235bfab526f6d
Pablo Lalloni <plalloni@gmail.com>
2018-07-30 01:18:32 -0300

    Install sdkman in $HOME

--- home/sdkman/install.sh
+++ home/sdkman/install.sh
@@ -1,3 +1,6 @@
-replacehomelinks \
-    ".sdkman" \
-    ".zsh/env.d/sdkman.zsh" \
+if [[ ! -d "~/.sdkman" ]]
+then
+    echo "Installing sdkman..."
+    curl -sSL "https://get.sdkman.io" | bash
+fi
+restow --no-folding .
