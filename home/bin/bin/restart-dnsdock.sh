d5c3bcaa2bcc6ba8d74e86a584c6975ef52498c9
Pablo Lalloni <plalloni@gmail.com>
2018-07-31 19:25:56 -0300

    Improve dnsdock setup with systemd-resolved

--- home/bin/bin/restart-dnsdock.sh
+++ home/bin/bin/restart-dnsdock.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/fish
 
-# After running this you need to add 127.0.0.54 to your list of DNS servers.
-#
-# Examples:
-#
-#  systemd-resolved: in /etc/systemd/resolved.conf, set DNS=127.0.0.54
-#  dnsmasq: in /etc/dnsmasq.d/dnsdock.conf, set server=/docker/127.0.0.54
-#  pure libc: in /etc/resolv.conf, set nameserver=127.0.0.54
-#  etc...
+function die
+    echo "aborting"
+    exit 1
+end
 
 if docker inspect dnsdock >/dev/null 2>&1
-    docker rm -rf dnsdock
+    docker rm -f dnsdock
+    or die
 end
 
 docker run -d \
@@ -20,3 +17,16 @@ docker run -d \
         -p 127.0.0.54:53:53/udp \
     aacebedo/dnsdock:latest-amd64 \
         --nameserver=(systemd-resolve --status --no-pager | grep -A 2 'DNS Servers:' | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | grep -v '^127\.')
+or die
+
+if not test -d /etc/systemd/resolved.conf.d
+    sudo mkdir /etc/systemd/resolved.conf.d
+    or die
+end
+
+echo '
+[Resolve]
+DNS=127.0.0.54
+' | sudo tee /etc/systemd/resolved.conf.d/dnsdock.conf >/dev/null
+and sudo systemctl daemon-reload
+and sudo systemctl restart systemd-resolved.service
