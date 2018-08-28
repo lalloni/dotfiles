ccfedb36eb2dd032509597c1c65339a905e0cf73
Pablo Lalloni <plalloni@gmail.com>
2018-08-28 16:52:49 -0300

    Fix nodenv installation link

--- home/nodenv/install.sh
+++ home/nodenv/install.sh
@@ -1,5 +1,6 @@
 replacehomelink ".nodenv"
+restow --no-folding "."
 if ! (cd ~/.nodenv && src/configure && make -C src)
 then
-    info "using nodenv not optimized"
+    info "using not-optimized nodenv"
 fi
