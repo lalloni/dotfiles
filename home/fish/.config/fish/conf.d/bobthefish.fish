f0f45b3ddde3a25731fbb3929c6a7023eade2187
Pablo Lalloni <plalloni@gmail.com>
2019-02-15 10:24:09 -0300

    Diseable k8s context info in fish prompt

--- home/fish/.config/fish/conf.d/bobthefish.fish
+++ home/fish/.config/fish/conf.d/bobthefish.fish
@@ -3,7 +3,7 @@ set -g theme_display_git_ahead_verbose yes
 set -g theme_display_git_dirty_verbose yes
 
 set -g theme_display_vagrant yes
-set -g theme_display_k8s_context yes
+set -g theme_display_k8s_context no
 set -g theme_display_user ssh
 
 set -g theme_show_exit_status yes
