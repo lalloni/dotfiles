d7ded28283b9e60879fd3041937d1c70d03ac234
Pablo Lalloni <plalloni@gmail.com>
2019-08-14 16:00:57 -0300

    Remove thefuck support

--- home/thefuck/install.sh
+++
@@ -1,8 +0,0 @@
-aptensurepkg virtualenv build-essential
-if [[ ! -d "$HOME/.py3env" ]]
-then
-    virtualenv --python=python3 "$HOME/.py3env"
-fi
-"$HOME/.py3env/bin/pip" install -U thefuck
-ln -frs "$HOME/.py3env/bin/fuck" "$HOME/bin/"
-ln -frs "$HOME/.py3env/bin/thefuck" "$HOME/bin/"
